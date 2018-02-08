f01d61059df9c0a43fe0b06b44810cb6dc56f69e
cookie1monster <11935636+cookie1monster@users.noreply.github.com>
2018-02-08 13:12:29 -0500

    Tree structure problems

--- src/org/problems/structure/PathSumBinaryTree.java
+++ src/org/problems/structure/PathSumBinaryTree.java
@@ -1,8 +1,5 @@
 package org.problems.structure;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //https://leetcode.com/problems/path-sum-iii/description/
 public class PathSumBinaryTree {
     public class TreeNode {
@@ -12,30 +9,20 @@ public class PathSumBinaryTree {
         TreeNode(int x) { val = x; }
     }
 
-    static public int pathSum(TreeNode root, int sum, List<Integer> list) {
-        list.add(root.val);
-        int localAmount = 0;
-        int localSum = 0;
-        for(int i=list.size()-1; i>=0; --i) {
-            localSum += list.get(i);
-            if (localSum == sum) {
-                localAmount++;
-            }
-        }
-        if (root.left != null) {
-            localAmount += pathSum(root.left, sum, list);
-        }
-        if (root.right != null) {
-            localAmount += pathSum(root.right, sum, list);
-        }
-        list.remove(list.size() - 1);
-
-        return localAmount;
+    static public int pathSubSum(TreeNode node, int sum) {
+        if (node == null)
+            return 0;
+        return (sum == node.val ? 1 : 0)
+                + pathSum(node.left, sum - node.val)
+                + pathSum(node.right, sum - node.val);
     }
 
     static public int pathSum(TreeNode root, int sum) {
-        if (root ==  null) return 0;
-        return pathSum(root, sum, new ArrayList<>());
+        if (root == null)
+            return 0;
+        return pathSubSum(root, sum)
+                + pathSum(root.left, sum)
+                + pathSum(root.right, sum);
     }
 
     public static void main(String[] args) {
