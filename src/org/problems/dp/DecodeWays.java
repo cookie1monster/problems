bffd90c95a9627772e6b518c40bf8b664cd09778
cookie1monster <11935636+cookie1monster@users.noreply.github.com>
2018-02-07 21:07:20 -0500

    DP problems

---
+++ src/org/problems/dp/DecodeWays.java
@@ -0,0 +1,30 @@
+package org.problems.dp;
+
+//https://leetcode.com/problems/decode-ways/description/
+public class DecodeWays {
+
+    static public int numDecodings(String s) {
+        if (s.length() == 0)
+            return 0;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        for(int i=0; i<s.length(); ++i) {
+            if (s.charAt(i) > '0' && s.charAt(i) <= '9') {
+                dp[i+1] = dp[i];
+            }
+            if (i > 0 && Integer.valueOf(s.substring(i-1,i+1)) > 9 && Integer.valueOf(s.substring(i-1,i+1)) < 27) {
+                dp[i+1] += dp[i-1];
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numDecodings("10"));
+        System.out.println(numDecodings("897"));
+        System.out.println(numDecodings("12"));
+        System.out.println(numDecodings("122"));
+        System.out.println(numDecodings("2212"));
+        System.out.println(numDecodings("12212"));
+    }
+}
---
+++ src/org/problems/dp/UniquePaths.java
@@ -0,0 +1,30 @@
+package org.problems.dp;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/unique-paths/description
+public class UniquePaths {
+
+    static public int uniquePaths(int m, int n) {
+        if (m==1 && n==1) return 1;
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) {
+            Arrays.fill(dp[i], 1);
+        }
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3,3));
+        System.out.println(uniquePaths(2,2));
+        System.out.println(uniquePaths(1,10));
+        System.out.println(uniquePaths(1,2));
+        System.out.println(uniquePaths(3,7));
+        System.out.println(uniquePaths(11,23));
+    }
+}
